3433e49b7da77859fc9cd50dc3cbd59c53ba5671
prabin singh <pravin544@gmail.com>
2020-08-01 19:28:18 -0500

    Added screen shot for read me page

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,19 @@
-var time = 
 $(document).ready(function() {
     
     const now = moment();
+
+    // Get current hour
     var currHour = now.format("hh A");
     
+    //Get current Date 
     var currDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a").toString();
+
     var savedHour = $(".row")
 
     //Add current date and time in header
     $("#currentDay").append(currDate);
 
+    //logic to change task block color
     var isFuture = false;
 
     $(".row").each(function(){
@@ -27,13 +31,14 @@ $(document).ready(function() {
         }
     });
 
+    //click event listner to retrieve hour, task and save in local storage.
     $(".saveBtn").on('click', function(){
         var saveBtn = $(this).attr("data-id");
         var clickedtime = ["09 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM","3 PM", "4 PM", "5 PM"];
         var savedTime = clickedtime[saveBtn];
         var task = $("#" + saveBtn).val();
         localStorage.setItem(savedTime,task);
-        })
+        });
     
 });
 
